574420f0ce597eb4c77ab4af99c1d980e7d4004a
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2024-01-29 19:14:06 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-""" holds class State"""
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-
-class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
-        @property
-        def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
-=======
 #!/usr/bin/python3
 """Defines the State class."""
 import models
@@ -69,4 +33,3 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
->>>>>>> 7ca55d2ae6b73d26fe12612842313d6a79f12b15
